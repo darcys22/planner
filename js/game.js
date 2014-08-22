49cde07afeca561d6f2d071eb6acf1924e3108d3
Sean Darcy <darcys22@gmail.com>
2014-08-22 00:26:46 -0700

    first commit, buttons and date

---
+++ js/game.js
@@ -0,0 +1,30 @@
+// Copyright © 2014 Demircan Celebi
+// Licensed under the terms of the MIT License
+
+var GameState = function(game) {};
+
+GameState.prototype.preload = function() {
+  game.load.image('clear','assets/clear.png');
+  game.load.image('previous','assets/previous.png');
+};
+
+var clear;
+var previous;
+
+GameState.prototype.create = function() {
+  this.game.stage.backgroundColor = 0x333333;
+
+  var date = "February 1st 2014"
+  var style = { font: "40px Arial", fill: "#9CA2B8" };
+  var tdate = game.add.text(10, 10, date, style);
+
+  previus = game.add.button(10, 65, 'previous');
+  clear = game.add.button(10, 110, 'clear');
+};
+
+GameState.prototype.update = function() {
+};
+
+// Setup game
+var game = new Phaser.Game(1136, 640, Phaser.AUTO);
+game.state.add('game', GameState, true);
