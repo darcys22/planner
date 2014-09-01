f8ae2398a0261a9aecb66a413c31a3a24a9a8d7f
Sean Darcy <darcys22@gmail.com>
2014-09-01 05:59:40 -0700

    physics is gay, why do they bounce

--- js/game.js
+++ js/game.js
@@ -71,10 +71,13 @@ GameState.prototype.box = function(x, y, size) {
   game.physics.arcade.enable(box);
   box.inputEnabled = true;
   box.input.enableDrag();
-  box.input.enableSnap(32,32,true,true);
+  box.input.enableSnap(71/2,40,true,false);
 
   box.body.collideWorldBounds = true;
+  box.body.mass = 10;
   //box.body.immovable = true;
+  //box.body.customSeparateX = true;
+  //box.body.customSeparateY = true;
 
   box.events.onDragStart.add(this.startDrag, this);
   box.events.onDragStop.add(this.stopDrag, this);
@@ -93,7 +96,9 @@ GameState.prototype.box = function(x, y, size) {
 //
 
 GameState.prototype.shiftAdd = function(sprite, pointer) {
-  this.box(this.game.input.x, this.game.input.y, 4);
+  var hour = Math.floor(this.game.input.x/71);
+  var xpos = hour * 71
+  this.box(xpos, this.game.input.y, 4);
 }
 GameState.prototype.startDrag = function(sprite, pointer) {
   sprite.body.moves = false;
