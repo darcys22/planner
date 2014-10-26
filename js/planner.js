b7051c08fbac6a9a12a7bf98d98d59593a86909a
Sean Darcy <darcys22@gmail.com>
2014-10-26 11:01:43 -0700

    more shit

--- js/planner.js
+++ js/planner.js
@@ -1,10 +1,13 @@
 var GameState = function(game) {};
+var SHIFT_SIZE = 71;
+var SHIFT_HEIGHT = 40;
 
 GameState.prototype.preload = function() {
   game.load.image('clear','assets/clear.png');
   game.load.image('previous','assets/previous.png');
   this.fallingShift = null;
   this.shiftGrid = [];
+  this.idCount = 1;
 };
 
 var clear;
@@ -75,14 +78,15 @@ GameState.prototype.ruler = function() {
 }
 
 GameState.prototype.box = function(x, y, size) {
-  var bmd = this.game.add.bitmapData(71*size, 40);
+  var bmd = this.game.add.bitmapData(shift.position/2 * SHIFT_SIZE, SHIFT_HEIGHT);
   bmd.context.fillStyle = 'rgba(255, 0, 0, 0.3)';
   roundRect(bmd.ctx, 0, 0, bmd.width, bmd.height, 5, true);
   var box = this.game.add.sprite(x,y,bmd);
+  box.id = shift.id;
 
   box.inputEnabled = true;
   box.input.enableDrag();
-  box.input.enableSnap(71/2,40,true,false);
+  box.input.enableSnap(SHIFT_SIZE/2, SHIFT_HEIGHT,true,false);
 
 
   box.events.onDragStart.add(this.startDrag, this);
@@ -95,7 +99,7 @@ GameState.prototype.box = function(x, y, size) {
 
   var speed = (this.game.height - 90 - box.y)*2;
 
-  this.boxTween = this.game.add.tween(box).to({ y: this.game.height - 90}, speed, Phaser.Easing.Linear.None, true)
+  this.boxTween = this.game.add.tween(box).to({ y: shift.height * SHIFT_HEIGHT}, speed, Phaser.Easing.Linear.None, true)
   this.boxTween.onComplete.add(fallin, this);
 
   this.fallingShift = box;
@@ -121,16 +125,20 @@ GameState.prototype.checkOverlap = function() {
     return ol;
 }
 
+//takes a shift, adds it to the shiftgrid
 GameState.prototype.addShiftGrid = function(shift) {
   position = this.checkGrid(shift);
   if (position == -1) {
     this.concatArr(this.shiftGrid, shift);
+    return this.shiftGrid.length;
   }
   else {
     this.ShiftGrid[position] = this.addShiftArray(this.ShiftGrid[position], shift)
   }
+  return position;
 }
 
+//Goes through the shift grid and returns the vertical array index the shift should be in (-1 if it cant fit)
 GameState.prototype.checkGrid = function(shift) {
   if (this.shiftGrid.length == 0) return -1;
   return this.shiftGrid.findIndex(function(x) {
@@ -138,13 +146,14 @@ GameState.prototype.checkGrid = function(shift) {
   }.first);
 }
 
-
+//creates a new empty single dimension array, then puts the shifts into it
 GameState.prototype.concatArr = function(arr, shift) {
   var empty = Array.apply(null, new Array(64)).map(Number.prototype.valueOf,0);
   this.addShiftArray(empty, shift)
   arr.push(empty);
 }
 
+// Given a single dimension array, go through it and add the shifts id to the hours time position
 GameState.prototype.addShiftArray = function(arr, shift) {
   for (var i = shift.position; i < shift.position + shift.length; i++)
   {
@@ -153,10 +162,13 @@ GameState.prototype.addShiftArray = function(arr, shift) {
   return arr;
 }
 
-GameState.prototype.Shift = function(id, position) {
-  this.position = position;
+//Shift object
+Shift = function(hour) {
+  this.position = hour*2;
   this.length = 8;
-  this.id = id;
+  this.id = idCount;
+  this.height = null;
+  idCount++;
 }
 
 
@@ -169,6 +181,8 @@ GameState.prototype.Shift = function(id, position) {
 
 GameState.prototype.shiftAdd = function(sprite, pointer) {
   var hour = Math.floor(this.game.input.x/71);
+  var shift = new Shift(hour);
+  shift.height = this.addShiftGrid(shift);
   var xpos = hour * 71
   this.box(xpos, this.game.input.y, 4);
 }
